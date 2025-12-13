17f0dfed08531302c2fc9a95baf9db78f655b956
ABHISHEK CHAUHAN <abhishekchauhan9760@gmail.com>
2025-12-13 15:52:34 +0530

    trust

---
+++ C++/pattern10.cpp
@@ -0,0 +1,30 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//      for (int row=1;row<=5;row++){
+        
+       
+//         for(int col=5;col>=row;col--){
+            
+//             cout<<'*'<<" ";
+            
+
+//         }
+//         cout<<endl;
+//      }
+// }
+#include<iostream>
+using namespace std;
+int main(){
+     for (int row=1;row<=5;row++){
+        
+       
+        for(int col=1;col<=5-(row-1);col++){
+            
+            cout<<'*'<<" ";
+            
+
+        }
+        cout<<endl;
+     }
+}
