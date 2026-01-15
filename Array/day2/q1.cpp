2e33895e3df5efcb5e62438f47431a8483228f51
abhi3114-glitch <abhishekchauhan9760@gmail.com>
2026-01-15 11:54:46 +0530

    trust me bro

---
+++ Array/day2/q1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={11,9,2,7,5};
+    for(int i=0;i<4;i++){
+        int index=i;
+
+    for(int j=i+1;j<5;j++){
+        if(arr[j]<arr[index]){
+            index=j;
+        }
+        
+    }
+    swap(arr[i],arr[index]);
+}
+for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    
+
+    }
+}
