1805ba8fa9d882513b62b760eec57c4431b4ff91
abhi3114-glitch <abhishekchauhan9760@gmail.com>
2026-01-15 14:39:22 +0530

    trust me bro

---
+++ Array/day2/q3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+    
+    int arr[n];
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    for(int i=n-2;i>=0;i--){
+        for(int j=0;j<=i;j++){
+            if (arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+    }
+    
+    }
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+}
